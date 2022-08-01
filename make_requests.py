2d33de4097350c4d319b11fb5d1d5cf94966f1fa
Thina <Thina@Zipcoders-MacBook-Pro-42.local>
2022-08-01 13:52:37 -0400

    relabeled file names

--- make_requests.py
+++ make_requests.py
@@ -7,7 +7,7 @@ file = 0
 while file < 39:
     url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?location&limit=1000&' + 'offset=' + str(offset_counter)
     req = urllib.request.Request(url, headers={'TOKEN': 'BwEQHKXofzVilsfljZDVTimCdklIiGLh'})
-    file_name = 'location_' + str(file) + '.json'
+    file_name = 'locations_' + str(file) + '.json'
     with urllib.request.urlopen(req) as f:
         data = json.load(f)
     with open(file_name, 'w') as w:
